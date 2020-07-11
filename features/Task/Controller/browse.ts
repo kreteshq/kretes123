0fc7fddcfbd0181d5f5e8a5a7107ab0b5ef48a0b
Zaiste <oh@zaiste.net>
2020-07-11 16:01:51 +0200

    migrate to 0.67.0

--- features/Task/Controller/browse.ts
+++ features/Task/Controller/browse.ts
@@ -2,9 +2,7 @@ import { Handler, response, database as db } from 'kretes';
 
 const { OK } = response;
 
-const browse: Handler = async () => {
+export const browse: Handler = async () => {
   const articles = await db.from('task');
   return OK(articles);
 }
-
-export = browse;
