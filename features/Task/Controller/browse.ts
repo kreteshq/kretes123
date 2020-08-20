2a6a0fd7fdbb793d01e9f738342123ed3294be0a
Zaiste <oh@zaiste.net>
2020-08-20 16:32:03 +0200

    fix the naming

--- features/Task/Controller/browse.ts
+++ features/Task/Controller/browse.ts
@@ -3,6 +3,7 @@ import { Handler, response, database as db } from 'kretes';
 const { OK } = response;
 
 export const browse: Handler = async () => {
-  const articles = await db.from('task');
-  return OK(articles);
+  const tasks = await db.from('task');
+
+  return OK(tasks);
 }
