161af5a0db993d1a0adb22ca81921abfb30817c6
Zaiste <oh@zaiste.net>
2020-09-05 19:42:08 +0200

    use OpenAPI integration

--- config/server/routes.ts
+++ config/server/routes.ts
@@ -6,7 +6,7 @@ const { Route: { GET, POST } } = routing;
 
 const routes: Routes = [
   // implicit `return` with a `text/plain` response
-  GET('/hello', _ => 'Hello, Kretes!'),
+  GET('/hello', _ => 'Hello, Kretes!', { meta: { summary: 'Hello' } }),
   GET('/json', _ => {
     // explicit `return` with a 200 response of `application/json` type
     return OK({ a: 1, b: 2 });
