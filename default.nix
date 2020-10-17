e5b8dd9864539496bdc19894da0c5583cdf948a1
Zaiste <oh@zaiste.net>
2020-10-17 23:05:43 +0200

    update Nix configuration

--- default.nix
+++ default.nix
@@ -1,14 +1,18 @@
 { pkgs ? import <nixpkgs> {}
 }:
 pkgs.mkShell {
-  name = "taskiapp-environment";
+  name = "kretes123-environment";
   buildInputs = [
+    pkgs.glibcLocales
     pkgs.nodejs-14_x
-    pkgs.postgresql_12
+    pkgs.postgresql_13
+    pkgs.pgcli
   ];
   shellHook = ''
+    export PGHOST=/tmp
     export PGDATA=./db/content
-    export PGPORT=5454
     export PGDATABASE=kretes123
+    export PGPORT=5454
+    export PGSSLMODE=disable
   '';
 }
